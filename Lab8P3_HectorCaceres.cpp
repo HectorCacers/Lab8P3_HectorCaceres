2276f1572e9f0b786250abf77941a5955436f2da
hector <hectorcacers11@gmail.com>
2023-03-10 14:07:14 -0600

    commit

--- Lab8P3_HectorCaceres.cpp
+++ Lab8P3_HectorCaceres.cpp
@@ -5,44 +5,8 @@
 #include <fstream>
 #include <string>
 #include <iomanip>
-
+#include "Concierto.h"
 using namespace std;
-
-class Concierto {
-private:
-    string nombre_banda;
-    double precio_entrada;
-    string fecha;
-    string codigo;
-    double total_recaudado;
-    int entradas_vendidas;
-
-public:
-    Concierto(string nombre, double precio, string fecha, string codigo) {
-        nombre_banda = nombre;
-        precio_entrada = precio;
-        this->fecha = fecha;
-        this->codigo = codigo;
-        total_recaudado = 0;
-        entradas_vendidas = 0;
-    }
-
-    void vender_entrada() {
-        total_recaudado += precio_entrada;
-        entradas_vendidas++;
-    }
-
-    int get_entradas_vendidas() {
-        return entradas_vendidas;
-    }
-
-    void guardar_concierto() {
-        ofstream archivo("conciertos.csv", ios::app);
-        archivo << nombre_banda << "," << fixed << setprecision(4) << precio_entrada << "," << fecha << "," << codigo << "," << entradas_vendidas << endl;
-        archivo.close();
-    }
-};
-
 int main() {
     // Ejemplo de uso
     Concierto concierto1("Banda1", 100.5678, "10/03/2023", "A12345");
